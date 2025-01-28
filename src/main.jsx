3c0f6fa5fcb379c40cbec860c3acbd65516f15fe
Rob-2021 <dragrober.rar@gmail.com>
2025-01-28 08:37:10 -0400

    Componentes

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { StoreApp } from './StoreApp'
+import NavBar from './NavBar'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <StoreApp
+        nombre={"Sucre"}
+        valor={"20 años de experiencia"}
+        />
+        <NavBar/>
+    </React.StrictMode>
+)
